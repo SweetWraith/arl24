16e66ee52310ea68dc0f6f2e4bd67947c94e5988
SweetWraith <84459623+SweetWraith@users.noreply.github.com>
2022-02-25 20:06:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+img = "";
+
+function setup(){
+ 
+}
+
+
+function draw(){
+ image(img,0,0,640,420);
+ fill("#FF0000");
+ text("Bed",110,75);
+ noFill();
+ stroke("#FF0000");
+ rect(100,60,450,350);
+}
